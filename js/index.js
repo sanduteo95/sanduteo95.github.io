8cf94a8da493e3e4cbb7eca4229bc3a990d471d9
Teodora Sandu <tas458@student.bham.ac.uk>
2016-12-17 19:09:17 +0200

    Added more icons and removed middle bit.

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
   $(window).resize(function() {
     var width = $('#img-middle').width();
-    var parentHeight= $('#someElt').offsetParent().height();
+    var parentHeight= $('#img-middle').offsetParent().height();
     var height = width;
     var heightPercentage = 100*height/parentHeight;
 
@@ -12,13 +12,10 @@ $(document).ready(function() {
     $('#img-bottom').css('height', ($('#img-middle').height()+30)+'px');
 
     width = width + 8*$(window).width()/100;
-    var angle = 240/4;
+    var angle = 180/($('#img-bottom img').length-1);
     var rot = 180;
 
-    console.log(width);
-
     $('#img-bottom img').map(function () {
-      var rotDegree = rot*Math.PI/180;
       var icon = $(this).get(0);
       $(icon).css({
         'color': 'red',
@@ -40,97 +37,44 @@ $(document).ready(function() {
 
   $(window).resize();
 
-  $("#img-1").hide();
-  $("#img-2").hide();
-  $("#img-3").hide();
-  $("#img-4").hide();
+  $('li').map(function () {
+    $(this).hide();
+  });
 
   setTimeout(function() {
     $('#img-middle').removeClass('img-pop-up');
-    $("#img-1").show();
-    $("#img-2").show();
-    $("#img-3").show();
-    $("#img-4").show();
+    $('li').map(function () {
+      $(this).show();
+    });
   }, 1000);
 
-  $('#img-1').on('click mouseover', function () {
-    $('#img-1').removeClass('img-blur-in');
+  $('#img-middle').hover(function() {
+    console.log('hey');
+    $('#more-info').show();
   });
 
-  $('#img-2').on('click mouseover', function () {
-    $('#img-2').removeClass('img-blur-in');
+  $('li').map(function () {
+    $(this).on('click mouseover', function () {
+      $(this).removeClass('img-blur-in');
+    });
   });
 
-  $('#img-3').on('click mouseover', function () {
-    $('#img-3').removeClass('img-blur-in');
-  });
-
-  $('#img-4').on('click mouseover', function () {
-    $('#img-4').removeClass('img-blur-in');
-  });
-
-  $('#img-1').click(function() {
+  $('#img-linkedin').click(function() {
     loadPage("https://uk.linkedin.com/in/teodora-sandu-b66b5b106");
   });
 
-  $('#img-2').click(function() {
+  $('#img-github').click(function() {
     loadPage("https://github.com/sanduteo95/");
   });
 
-  $('#img-3').click(function() {
-    if($('#more-info').hasClass('info-3')) {
-      $('#img-middle').toggle();
-      $('#more-info').toggle();
-    } else {
-      if(!$('#more-info').hasClass('info-4')) {
-        $('#img-middle').toggle();
-        $('#more-info').toggle();
-      } else {
-        $('#more-info').removeClass('info-4');
-      }
-      loadContactDetails();
-    }
+  $('#img-facebook').click(function() {
+    loadPage("https://www.facebook.com/sanduteo95");
   });
 
-  $('#img-4').click(function() {
-    if($('#more-info').hasClass('info-4')) {
-      $('#img-middle').toggle();
-      $('#more-info').toggle();
-    } else {
-      if(!$('#more-info').hasClass('info-3')) {
-        $('#img-middle').toggle();
-        $('#more-info').toggle();
-      } else {
-        $('#more-info').removeClass('info-3');
-      }
-      loadCV();
-    }
+  $('#img-twitter').click(function() {
+    loadPage("https://twitter.com/sanduteo95");
   });
-
 });
-
-function loadContactDetails() {
-  var target = $('#more-info');
-
-  target.addClass('info-3');
-
-  var html = '';
-  html += '<p class="info-text"><i class="fa fa-facebook-official" style="color: black;"></i><a href="https://www.facebook.com/sanduteo95" target="_blank"> Facebook </a></i></p>';
-  html += '<p class="info-text"><i class="fa fa-twitter" style="color: black;"></i><a href="https://twitter.com/sanduteo95"> Twitter <a/></i></p>';
-  html += '<p class="info-text"><i class="fa fa-envelope" style="color: black;"></i><a href="mailto:sanduteo95@gmail.com">sanduteo95@gmail.com</a></i></p>';
-
-  target.html(html);
-}
-
-function loadCV() {
-  var target = $('#more-info');
-
-  target.addClass('info-4');
-  var html = '';
-  html += '<p class="info-text"><i class="fa fa-file-text-o"><a href="../CV.pdf" download="CV Teodora Sandu">Download my Resume</a></i></p>';
-
-  target.html(html);
-}
 
 function loadPage(page) {
   var win = window.open(page, '_blank');
